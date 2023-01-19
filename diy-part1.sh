977aacac4c4596a0232a5e975be2fa6414a09cfe
viogus <zhixun.lin@gmail.com>
2023-01-19 22:51:20 +0800

    add realtek r8125 2.5G Ethernet kernel modules

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,8 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# r8125 kernel module
+# get the source 
+git clone https://github.com/awesometic/realtek-r8125-dkms.git 
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+make -C . M=./realteck-r8125-dkms/src modules_install
