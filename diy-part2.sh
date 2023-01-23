59d3107c0889303ea2f66d7fc5d7ccaf11389bee
viogus <zhixun.lin@gmail.com>
2023-01-23 09:36:10 +0800

    use github src

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,11 +21,4 @@ xz -z ./realtek-r8125-dkms/src/r8125.ko
 cp ./realtek-r8125-dkms/src/r8125.ko.xz /lib/modules/${KERNELRELEASE}/kernel/drivers/net/ethernet/realtek/
 
 # nct6687d kernel module
-git clone https://github.com/Fred78290/nct6687d.git
-mkdir nct6687d/build
-cp nct6687d/Makefile nct6687d/nct6687.c nct6687d/build
-cd nct6687d/build
-make -C /lib/modules/${KERNELRELEASE}/build M=. modules
-xz -z ./nct6687.ko
-cp ./nct6687.ko.xz /lib/modules/${KERNELRELEASE}/kernel/drivers/hwmon/
-
+# Create necessary directories and clone repository
