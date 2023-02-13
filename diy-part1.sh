634a76dd119680c73bfeca4ce0de40565621b0d2
viogus <zhixun.lin@gmail.com>
2023-02-13 08:26:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,5 +11,13 @@
 #
 git checkout 20221220
 cp unraid_6.11.1_conf_regen-6.1-vendor-reset-gcc .config
+echo "CONFIG_CFG80211=m
+CONFIG_MAC80211=m
+CONFIG_WLAN_VENDOR_INTEL=y
+CONFIG_IWLWIFI=m
+CONFIG_IWLWIFI_LEDS=y
+CONFIG_IWLWIFI_OPMODE_MODULAR=y
+CONFIG_IWLDVM=m
+CONFIG_IWLMVM=m" >> .config
 wget https://cdn.kernel.org/pub/linux/kernel/v6.x/patch-6.1.11.xz
 xzcat patch-6.1.*.xz | patch -p1
