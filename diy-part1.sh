e799d711b9e3c4aba28e444c9955107b13d8c1d9
viogus <zhixun.lin@gmail.com>
2023-02-26 23:13:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,14 +13,5 @@ git checkout 20221220
 
 cp unraid_6.11.1_conf_regen-6.1-vendor-reset-gcc .config
 sed -i '/WLAN_VEN/d' .config
-echo "CONFIG_CFG80211=m
-CONFIG_MAC80211=m
-CONFIG_WLAN_VENDOR_INTEL=y
-CONFIG_IWLWIFI=m
-CONFIG_IWLWIFI_LEDS=y
-CONFIG_IWLWIFI_OPMODE_MODULAR=y
-CONFIG_IWLDVM=m
-CONFIG_IWLMVM=m" >> .config
-
 wget https://cdn.kernel.org/pub/linux/kernel/v6.x/patch-6.1.14.xz
 xzcat patch-6.1.*.xz | patch -p1
