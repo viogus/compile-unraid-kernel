6445772a5f348d60f1c3123810ac71ebde0cf33a
viogus <zhixun.lin@gmail.com>
2023-03-12 22:33:57 +0800

    6.2.5

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,5 +13,5 @@ git checkout 20230220
 
 cp unraid_6.11.1_conf_regen-6.2-vendor-reset-gcc .config
 sed -i '/WLAN_VEN/d' .config
-wget https://cdn.kernel.org/pub/linux/kernel/v6.x/patch-6.2.2.xz
+wget https://cdn.kernel.org/pub/linux/kernel/v6.x/patch-6.2.5.xz
 xzcat patch-6.2.*.xz | patch -p1
