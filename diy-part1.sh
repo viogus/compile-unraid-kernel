1271576f678a1cb838abab0ada722a3ade929b10
viogus <zhixun.lin@gmail.com>
2023-03-12 18:47:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,4 @@ git checkout 20221220
 cp unraid_6.11.1_conf_regen-6.1-vendor-reset-gcc .config
 sed -i '/WLAN_VEN/d' .config
 wget https://cdn.kernel.org/pub/linux/kernel/v6.x/patch-6.1.18.xz
-xzcat patch-6.1.*.xz | git apply patch -p1 --exclude=drivers/pci/quirks.c
+xzcat patch-6.1.*.xz | git apply-patch -p1 --exclude=drivers/pci/quirks.c
