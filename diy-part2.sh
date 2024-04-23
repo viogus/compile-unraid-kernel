36d83c30fbfa606212227431d39684ab506162f9
viogus <zhixun.lin@gmail.com>
2024-04-23 22:15:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,11 +14,11 @@ KERNELRELEASE=$(cat ./include/config/kernel.release)
 
 # r8125 kernel module
 # get the source 
-git clone https://github.com/awesometic/realtek-r8125-dkms.git 
-make -C . M=./realtek-r8125-dkms/src modules
+#git clone https://github.com/awesometic/realtek-r8125-dkms.git 
+#make -C . M=./realtek-r8125-dkms/src modules
 # xz
-xz -z ./realtek-r8125-dkms/src/r8125.ko
-cp ./realtek-r8125-dkms/src/r8125.ko.xz /lib/modules/${KERNELRELEASE}/kernel/drivers/net/ethernet/realtek/
+#xz -z ./realtek-r8125-dkms/src/r8125.ko
+#cp ./realtek-r8125-dkms/src/r8125.ko.xz /lib/modules/${KERNELRELEASE}/kernel/drivers/net/ethernet/realtek/
 
 # nct6687d kernel module
 # Create necessary directories and clone repository
